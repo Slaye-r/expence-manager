1369837b73cf0eeea6cc71e3db0762c0722507f8
Slaye-r <111708118+Slaye-r@users.noreply.github.com>
2022-12-15 15:59:35 +0530

    upadeted

--- src/App.js
+++ src/App.js
@@ -5,21 +5,21 @@ import { useState } from "react";
 const DUMMY__EXPENSES = [
   {
     id: "e1",
-    title: "Toilet Paper",
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
+    title: "Books",
+    amount: 394.12,
+    date: new Date(2022, 7, 14),
   },
-  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  { id: "e2", title: "Laptop", amount: 57799.49, date: new Date(2021, 2, 12) },
   {
     id: "e3",
-    title: "Car Insurance",
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
+    title: "Mess Fess",
+    amount: 12294.67,
+    date: new Date(2022, 2, 28),
   },
   {
     id: "e4",
     title: "New Desk (Wooden)",
-    amount: 450,
+    amount: 750,
     date: new Date(2021, 5, 12),
   },
 ];
@@ -28,7 +28,7 @@ function App() {
   const [expenses, setExpenses] = useState(DUMMY__EXPENSES);
 
   const addExpenseHandeler = (expense) => {
-    setExpenses(prevExpenses => {
+    setExpenses((prevExpenses) => {
       return [expense, ...prevExpenses];
     });
   };
